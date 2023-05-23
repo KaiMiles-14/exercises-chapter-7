64811c3f36f9d623bd590dbf9814bd126451ee3e
Kai Miles <kgm221@ic.ac.uk>
2023-05-23 05:35:06 +0100

    2

---
+++ groups/symmetric_groups.py
@@ -0,0 +1,20 @@
+import numpy as np
+from .groups import Group
+
+class SymmetricGroup(Group):
+    symbol = "S"
+
+    def __init__(self, n):
+        super().__init__()
+        self.n = n
+
+    def validate_element(self, element):
+        if not isinstance(element, np.ndarray):
+            raise ValueError("Group element must be a numpy.ndarray.")
+        if len(element) != self.n:
+            raise ValueError(f"Group element must have length {self.n}.")
+
+    def group_operation(self, a, b):
+        self.validate_element(a)
+        self.validate_element(b)
+        return a[b]
